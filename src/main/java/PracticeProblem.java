8695b3293d0dc41b4b3980916b721c6dc105f4dd
karthikeya-arikirevula-ics4u1-02-25 <karthikeya.arikirevula@student.tdsb.on.ca>
2025-02-05 14:59:21 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -10,33 +12,61 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
-		
+		Scanner in = new Scanner(System.in);
+		String question1;
+		System.out.print("Input a word: ");
+		question1 = in.nextLine();
+		System.out.println(question1 + "\n");	
 	}
 
 	public static void q2() {
-		//Write question 2 code here
-		
+		Scanner in = new Scanner(System.in);
+		int int1;
+		int int2;
+		System.out.print("Input an integer: ");
+		int1 = in.nextInt();
+		in.nextLine();
+		System.out.print("Input another integer: ");
+		int2 = in.nextInt();
+		in.nextLine();
+		System.out.println("The quotient of your two numbers is: " + (int1/int2));
+		System.out.println("The remainder of that quotient is: " + (int1%int2) + "\n");
 	}
 
 	public static void q3() {
-		//Write question 3 code here
-		
+		Scanner in = new Scanner(System.in);
+		String question1;
+		System.out.print("Input a word: ");
+		question1 = in.nextLine();
+		System.out.println(question1 + " was the text you wrote" + "\n");	
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner in = new Scanner(System.in);
+		int int3;
+		System.out.print("Enter an integer: ");
+		int3 = in.nextInt();
+		in.nextLine();
+		System.out.println("The integer you said, multiplied by 5, is: " + (int3 * 5) + "\n");
 		
 	}
 
 	public static void q5() {
-		//Write question 5 code here
-		
+		Scanner in = new Scanner(System.in);
+		boolean boolean1;
+		System.out.print("Enter a boolean: ");
+		boolean1 = in.nextBoolean();
+		in.nextLine();
+		System.out.println(boolean1 + " is a boolean" + "\n");
 	}
 
 	public static void q6() {
-		//Write question 6 code here
-		
+		Scanner in = new Scanner(System.in);
+		double double1;
+		System.out.print("Enter a double: ");
+		double1 = in.nextDouble();
+		in.nextLine();
+		System.out.println("The double you said, subtracted by 3.2, is: " + (double1 - 3.2) + "\n");
 	}
 
 }
